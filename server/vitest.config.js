311c084a2557d65a040cac12fe687da45c3c46a7
tshi13 <tshi13@jh.edu>
2024-04-23 15:18:21 -0400

    fixed server bug

--- server/vitest.config.js
+++ server/vitest.config.js
@@ -4,13 +4,14 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+		coverage: {
+			provider: "v8", // specify the coverage provider, c8 is the default for Vitest
+			include: ["server/controllers/**/*.js"], // target all JavaScript files within the controllers directory
+			exclude: ["node_modules/**", "tests/**", "*.config.js"], // exclude node_modules, test files, and config files from coverage
+			reportsDirectory: "./coverage", // specify where to output coverage reports
+			reporter: ["text", "html"], // specify types of coverage reports to generate
+			all: true, // whether to include all files in the coverage report, not just files that have tests
+			enabled: true,
+		},
   },
-  // coverage: {
-  //   provider: "c8", // specify the coverage provider, c8 is the default for Vitest
-  //   include: ["server/controllers/**/*.js"], // target all JavaScript files within the controllers directory
-  //   exclude: ["node_modules/**", "tests/**", "*.config.js"], // exclude node_modules, test files, and config files from coverage
-  //   reportsDirectory: "./coverage", // specify where to output coverage reports
-  //   reporter: ["text", "html"], // specify types of coverage reports to generate
-  //   all: true, // whether to include all files in the coverage report, not just files that have tests
-  // },
 });
