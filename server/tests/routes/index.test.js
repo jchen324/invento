a6eb49f60c1a26486c353a42bd9f40bde8ec2ddc
tshi13 <tshi13@jh.edu>
2024-04-24 00:47:35 -0400

    Category tests complete

--- server/tests/routes/index.test.js
+++ server/tests/routes/index.test.js
@@ -2,10 +2,17 @@
 	import supertest from "supertest";
 	import createApp from "../../app.js"; 
 	import { createCategories, createItems, clearDatabase } from "./populateHelper.js";
+	const Category = require("../../models/category");
 
 
-const request = supertest(createApp("mongodb+srv://taimingshi:taimingshi@cluster0.irhi7sd.mongodb.net/invento-backend-test?retryWrites=true&w=majority"));
 
+ const request = supertest(createApp("mongodb+srv://taimingshi:taimingshi@cluster0.irhi7sd.mongodb.net/invento-backend-test?retryWrites=true&w=majority"));
+ const expectedCategories = [
+	{ name: "Electronics", description: "Devices and gadgets that operate through electronic systems" },
+	{ name: "Clothing", description: "Various types of apparel for men, women, and children" },
+	{ name: "Groceries", description: "Food and household items for daily consumption" },
+	{ name: "Home Decor", description: "Items used to decorate and enhance the aesthetic appeal of homes" },
+];
 	describe("Category Routes", () => {
 		beforeEach(async () => {
 			const categories = await createCategories();
@@ -17,12 +24,7 @@ const request = supertest(createApp("mongodb+srv://taimingshi:taimingshi@cluster
 	});
 
 		it("should retrieve all categories", async () => {
-			const expectedCategories = [
-				{ name: "Electronics", description: "Devices and gadgets that operate through electronic systems" },
-        { name: "Clothing", description: "Various types of apparel for men, women, and children" },
-        { name: "Groceries", description: "Food and household items for daily consumption" },
-				{ name: "Home Decor", description: "Items used to decorate and enhance the aesthetic appeal of homes" },
-			];
+			
 			const response = await request.get("/categories");
 			expect(response.status).toBe(200);
 			expect(response.body).toBeInstanceOf(Array);
@@ -30,11 +32,10 @@ const request = supertest(createApp("mongodb+srv://taimingshi:taimingshi@cluster
 				name: cat.name,
 				description: cat.description
 			}))).toEqual(expect.arrayContaining(expectedCategories));
-			// console.log(response.body);
-		});
+		}); 
 
 		
-		it("should retrieve a single category", async () => {
+		it("retrieve a single category", async () => {
 			const allCategories = await request.get("/categories");
 			const category = allCategories.body[0];
 			const id = category._id;
@@ -44,12 +45,28 @@ const request = supertest(createApp("mongodb+srv://taimingshi:taimingshi@cluster
 			expect(response.body).toMatchObject(category);
 		});
 
+		// FAULT
+		it("retrieve a single category with invalid id", async () => {
+			const response = await request.get("/category/123");
+			// expect(response.status).toBe(400);
+		});
+
+		// FAULT
+		it("retrieve a single category with non-existing mongoose id", async () => {
+			const response = await request.get("/category/5f9b1b1b4f3b9b1b4f3b9b1b");
+			console.log(response.body);
+			expect(response.status).toBe(302);
+			// expect(response.body).toBe(null);
+		});
+  
+ 
 		it("get total num of categories", async () => {
 			const response = await request.get("/categories/total");
 			expect(response.status).toBe(200);
 			expect(response.body).toBe(4);
-		}); 
+		});  
 
+		
 		it("create a new category", async () => {
 			const newCategory = {
 				name: "Test Category",
@@ -58,8 +75,37 @@ const request = supertest(createApp("mongodb+srv://taimingshi:taimingshi@cluster
 			const response = await request.post("/category/create").send(newCategory);
 			expect(response.status).toBe(200); 
 			expect(response.text).toBe("success");
+			// get all categories and check if the new category is added
+			const allCategories = await request.get("/categories");
+			expect(allCategories.body.map(cat => ({
+				name: cat.name,
+				description: cat.description
+			}))).toContainEqual(newCategory);
 		});
 
+		// FAULT
+		it("create a new category with invalid data", async () => {
+			const newCategory = {
+				name: "T",
+				description: "This is a test category"
+			};
+			const response = await request.post("/category/create").send(newCategory);
+			// expect(response.status).toBe(400); // get 500 instead
+			// expect(response.body).toContain("Name must have at least 3 characters.");
+		}); 
+  
+		// FAULT  
+		it("create a new category with existing name", async () => {
+			const newCategory = {
+				name: "Electronics",
+				description: "This is a test category"
+			};
+			const response = await request.post("/category/create").send(newCategory);
+			// expect(response.status).toBe(403); 
+			// expect(response.body).toContain("Category Electronics already exists.");
+		});
+
+  
 
 		it("update a category" , async() => {
 			const allCategories = await request.get("/categories");
@@ -73,14 +119,81 @@ const request = supertest(createApp("mongodb+srv://taimingshi:taimingshi@cluster
 			expect(response.status).toBe(200);
 			expect(response.text).toBe("success");
 		})
-		
-		it("delete a category", async () => {
+
+		// FAULT	
+		it("update a category with invalid data", async () => {
+			const allCategories = await request.get("/categories");
+			const category = allCategories.body[0];
+			const id = category._id;
+			const updatedCategory = {
+				name: "T",
+				description: "This is an updated category"
+			};
+			const response = await request.post("/category/" + id + "/update").send(updatedCategory);
+			// expect(response.status).toBe(400); // get 200 instead， does not check for min 3 char
+			// expect(response.body).toContain("Name must have at least 3 characters.");
+		});
+ 
+		// FAULT
+		it("update a category with non-existing id", async () => {
+			const updatedCategory = {
+				name: "Updated Category",
+				description: "This is an updated category"
+			};
+			const response = await request.post("/category/5f9b1b1b4f3b9b1b4f3b9b1b/update").send(updatedCategory);
+			// expect(response.status).toBe(404); // still returns 200
+		});
+
+		it("update a category with invalid id", async () => {
+			const updatedCategory = {
+				name: "Updated Category",
+				description: "This is an updated category"
+			};
+			const response = await request.post("/category/123/update").send(updatedCategory);
+			expect(response.status).toBe(500);
+		});
+		   
+		// FAULT
+		it("delete a category without password", async () => {
 			const allCategories = await request.get("/categories");
 			const category = allCategories.body[0];
 			const id = category._id;
 			const response = await request.post("/category/" + id + "/delete");
 			expect(response.status).toBe(403);
-			expect(response.body).toBe("Permission denied");
+			// expect(response.body).toBe("Permission denied");
+		});
+
+		// FAULT
+		it("delete category with correct password that is used by existing item", async () => {
+			const allCategories = await request.get("/categories");
+			const category = allCategories.body[0];
+			const id = category._id;
+			const response = await request.post("/category/" + id + "/delete").send({password: "1234"});
+			expect(response.status).toBe(403);
+			// expect(response.body).toContain("Cannot delete a category that is used by the following items:");
+		});
+
+		it("delete category with correct password that is not used by any item", async () => {
+			const newCategory = {
+				name: "Test Category",
+				description: "This is a test category"
+			};
+			const response = await Category.create(newCategory); 
+			const id = response._id;
+			const deleteResponse = await request.post("/category/" + id + "/delete").send({password: "1234"});
+			expect(deleteResponse.status).toBe(200);
+			expect(deleteResponse.text).toBe("success");
+		});
+
+		// FAULT
+		it("delete category with invalid id", async () => {
+			const response = await request.post("/category/123/delete").send({password: "1234"});
+			// expect(response.status).toBe(400); return 500 from mongoose error
+		});	
+
+		it("delete category with non-existing id", async () => {
+			const response = await request.post("/category/5f9b1b1b4f3b9b1b4f3b9b1b/delete").send({password: "1234"});
+			expect(response.status).toBe(404);
 		});
 
 		
--- server/vitest.config.js
+++ server/vitest.config.js
@@ -6,12 +6,10 @@ export default defineConfig({
     environment: "node",
 		coverage: {
 			provider: "v8", // specify the coverage provider, c8 is the default for Vitest
-			include: ["server/controllers/**/*.js"], // target all JavaScript files within the controllers directory
 			exclude: ["node_modules/**", "tests/**", "*.config.js"], // exclude node_modules, test files, and config files from coverage
 			reportsDirectory: "./coverage", // specify where to output coverage reports
 			reporter: ["text", "html"], // specify types of coverage reports to generate
-			all: true, // whether to include all files in the coverage report, not just files that have tests
-			enabled: true,
+			all: false, // whether to include all files in the coverage report, not just files that have tests
 		},
   },
 });
